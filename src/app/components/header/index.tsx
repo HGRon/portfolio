0898aae3f45ad24b6122db8a8a1d606bed67d811
Higor Lins <higor.lins@facens.br>
2024-11-15 16:44:40 -0300

    refactor(menu.enum): add menu enum to types

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,14 +3,8 @@
 import React, { useEffect, useState } from 'react'
 import Icon from '../icon'
 import Logo from '@/app/components/logo'
-import Link from 'next/link';
-
-export enum Menu {
-  HomeCover = 'homeCover',
-  HomeAboutMe = 'homeAboutMe',
-  HomeProjects = 'homeProjects',
-  HomeContactMe = 'homeContactMe',
-}
+import Link from 'next/link'
+import { Menu } from '@/app/types/menu.enum'
 
 export function MenuIcon({
   icon,
@@ -32,7 +26,7 @@ export function MenuIcon({
 }
 
 export default function Header() {
-  const [activatedMenu, setActivatedMenu] = useState(Menu.HomeCover)
+  const [activatedMenu, setActivatedMenu] = useState(Menu.HOME_COVER)
 
   useEffect(() => {
     const scroller = document.getElementById('homeScroller')
@@ -45,16 +39,16 @@ export default function Header() {
         const homeContactMe = document.getElementById('homeContactMe')
 
         if (scroller.scrollTop > homeCover!.offsetTop - 100)
-          setActivatedMenu(Menu.HomeCover)
+          setActivatedMenu(Menu.HOME_COVER)
 
         if (scroller.scrollTop > homeAboutMe!.offsetTop - 100)
-          setActivatedMenu(Menu.HomeAboutMe)
+          setActivatedMenu(Menu.HOME_ABOUT_ME)
 
         if (scroller.scrollTop > homeProjects!.offsetTop - 100)
-          setActivatedMenu(Menu.HomeProjects)
+          setActivatedMenu(Menu.HOME_PROJECTS)
 
         if (scroller.scrollTop > homeContactMe!.offsetTop - 100)
-          setActivatedMenu(Menu.HomeContactMe)
+          setActivatedMenu(Menu.HOME_CONTACT_ME)
       })
     }
   }, [])
@@ -62,22 +56,39 @@ export default function Header() {
   return (
     <header className="flex bg-white/40 backdrop-blur-[1px] justify-center items-center md:w-without-scrollbar w-full fixed top-0 left-0 z-50">
       <div className="flex w-full justify-between items-center container-size">
-        <Logo/>
+        <Logo />
 
         <div className="flex items-center gap-2">
           <Link
             href="/cv_higor_lins.pdf"
             target="_blank"
-            className="flex gap-2 items-center h-fit justify-center px-4 py-2 bg-[#171717] rounded-full cursor-pointer">
+            className="flex gap-2 items-center h-fit justify-center px-4 py-2 bg-[#171717] rounded-full cursor-pointer"
+          >
             <p className="text-base font-light text-white">CV</p>
-            <Icon className="text-white" icon="download"/>
+            <Icon className="text-white" icon="download" />
           </Link>
 
           <div className="hidden md:flex p-2 gap-2 bg-[#171717] rounded-full">
-            <MenuIcon icon="home" anchor={Menu.HomeCover} selected={activatedMenu === Menu.HomeCover}/>
-            <MenuIcon icon="face" anchor={Menu.HomeAboutMe} selected={activatedMenu === Menu.HomeAboutMe}/>
-            <MenuIcon icon="backup_table" anchor={Menu.HomeProjects} selected={activatedMenu === Menu.HomeProjects}/>
-            <MenuIcon icon="mail" anchor={Menu.HomeContactMe} selected={activatedMenu === Menu.HomeContactMe}/>
+            <MenuIcon
+              icon="home"
+              anchor="homeCover"
+              selected={activatedMenu === Menu.HOME_COVER}
+            />
+            <MenuIcon
+              icon="face"
+              anchor="homeAboutMe"
+              selected={activatedMenu === Menu.HOME_ABOUT_ME}
+            />
+            <MenuIcon
+              icon="backup_table"
+              anchor="homeProjects"
+              selected={activatedMenu === Menu.HOME_PROJECTS}
+            />
+            <MenuIcon
+              icon="mail"
+              anchor="homeContactMe"
+              selected={activatedMenu === Menu.HOME_CONTACT_ME}
+            />
           </div>
         </div>
       </div>
---
+++ src/app/types/menu.enum.ts
@@ -0,0 +1,6 @@
+export enum Menu {
+  HOME_COVER = 'home_cover',
+  HOME_ABOUT_ME = 'home_about_me',
+  HOME_PROJECTS = 'home_projects',
+  HOME_CONTACT_ME = 'home_contact_me',
+}
