a9d9d1bf85d675be44fd3d6e3673f431879c2054
Higor Lins <higor.lins@facens.br>
2024-11-22 21:14:19 -0300

    feature(home): add profession component in start of home

--- src/app/[lng]/home/components/presentation/index.tsx
+++ src/app/[lng]/home/components/presentation/index.tsx
@@ -1,11 +1,13 @@
 import HelloPill from '@/app/[lng]/home/components/hello-pill'
 import MyName from '@/app/[lng]/home/components/my-name'
+import Profession from '@/app/[lng]/home/components/profession';
 
 export default function Presentation() {
   return (
     <div className="flex items-center flex-col mt-36 animation-to-down">
       <HelloPill />
       <MyName />
+      <Profession />
     </div>
   )
 }
---
+++ src/app/[lng]/home/components/profession/index.tsx
@@ -0,0 +1,7 @@
+export default function Profession() {
+  return (
+    <p className="text-primary text-md font-bold text-center mt-4 w-full">
+      F U L L S T A C K @ D E V E L O P E R
+    </p>
+  )
+}
